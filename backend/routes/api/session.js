9b4755b154e4d8a42897ba3515d7a47be41f29d4
Andrew Stilinovic <stili87@gmail.com>
2022-05-25 18:13:09 -0500

    added validation to utils

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,9 +10,7 @@ router.post(
     '/',
     asyncHandler(async (req, res, next) => {
       const { credential, password } = req.body;
-  
       const user = await User.login({ credential, password });
-  
       if (!user) {
         const err = new Error('Login failed');
         err.status = 401;
@@ -20,9 +18,7 @@ router.post(
         err.errors = ['The provided credentials were invalid.'];
         return next(err);
       }
-  
       await setTokenCookie(res, user);
-  
       return res.json({
         user
       });
@@ -34,6 +30,19 @@ router.post(
     (_req, res) => {
       res.clearCookie('token');
       return res.json({ message: 'success' });
+    }
+  );
+
+  router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
     }
   );
 

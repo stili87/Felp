b2505ed7f6490da61e56af87e3af38bddb7a9973
Andrew Stilinovic <stili87@gmail.com>
2022-05-25 17:38:42 -0500

    finsihed phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', csrfProtection, function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());

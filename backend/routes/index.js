19ae6f32e9a154090b6b68493e882d082aaddba5
Andrew Stilinovic <stili87@gmail.com>
2022-05-25 16:26:03 -0500

    added api routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
+const apiRouter = require('./api');
 
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', csrfProtection, function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());

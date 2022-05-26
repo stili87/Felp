b028acd746a921c5ea5cdafb6634fd3536c0bd76
Andrew Stilinovic <stili87@gmail.com>
2022-05-26 14:41:29 -0500

    finished part 2 of authenticateme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,41 +1,32 @@
-import React from 'react';
-import './index.css';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import configureStore from './store';
-import * as sessionActions from './store/session';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
+
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
 }
 
-
-if (process.env.NODE_ENV !== 'production') {
-  restoreCSRF();
-  window.csrfFetch = csrfFetch;
-  window.store = store;
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
-
-
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
@@ -44,5 +35,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );

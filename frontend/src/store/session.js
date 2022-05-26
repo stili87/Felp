2374e910ae9d54dc52912327b0f107923446ada9
Andrew Stilinovic <stili87@gmail.com>
2022-05-26 14:32:56 -0500

    finished Nav bar with profile button and logout

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,6 +17,14 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 export const signup = (user) => async (dispatch) => {
   const { username, email, password } = user;
   const response = await csrfFetch("/api/users", {

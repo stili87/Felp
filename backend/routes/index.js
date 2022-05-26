81b9b51298b9e486c342b9158bf4459a2c96a15e
Andrew Stilinovic <stili87@gmail.com>
2022-05-26 15:17:04 -0500

    fixed csurf

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,7 +11,7 @@ router.use('/api', apiRouter);
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   // Serve the frontend's index.html file at the root route
-  router.get('/', csrfProtection, (req, res) => {
+  router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   router.use(express.static(path.resolve("../frontend/build")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
-  router.get(/^(?!\/?api).*/, csrfProtection, (req, res) => {
+  router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
